ed1f165018fa71c73171ae82504387632f3bc805
Kamil <Kamil@DESKTOP-M0B1PRQ>
2021-04-27 20:46:15 +0200

    Static views and store controller added.

--- SpodIgly/Controllers/StoreController.cs
+++ SpodIgly/Controllers/StoreController.cs
@@ -6,17 +6,22 @@ using System.Web.Mvc;
 
 namespace SpodIgly.Controllers
 {
-    public class HomeController : Controller
+    public class StoreController : Controller
     {
-        // GET: Home
+        // GET: Store
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult StaticContent(string viewname)
+        public ActionResult Details(int id)
         {
-            return View(viewname);
+            return View();
+        }
+
+        public ActionResult List(string genrename)
+        {
+            return View();
         }
     }
 }
