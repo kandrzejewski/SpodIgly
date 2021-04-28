33c665a8d0fbecbb7533e52e992044f34704ea58
Kamil <Kamil@DESKTOP-M0B1PRQ>
2021-04-28 21:47:12 +0200

    First data models added.

---
+++ SpodIgly/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpodIgly.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string CodeAndCity { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+        public DateTime DateCreated { get; set; }
+        public OrderState OrderState { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+
+    public enum OrderState
+    {
+        New,
+        Shipped
+    }
+}
