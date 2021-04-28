1ff79fa4aeccc66fa74a4ada87c0b2df2b312489
Kamil <Kamil@DESKTOP-M0B1PRQ>
2021-04-28 21:49:36 +0200

    Set up Entity Framework and db context.

---
+++ SpodIgly/DAL/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using SpodIgly.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SpodIgly.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext() :base("StoreContext")
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+    }
+}
