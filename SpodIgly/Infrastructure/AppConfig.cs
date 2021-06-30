508447550a2dce9e4a7095a97f115d802631ba78
Kamil <Kamil@DESKTOP-M0B1PRQ>
2021-06-30 23:30:00 +0200

    UrlHelper extensions for content added.

---
+++ SpodIgly/Infrastructure/AppConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SpodIgly.Infrastructure
+{
+    public class AppConfig
+    {
+        private static string _genreIconsFolderRelative = ConfigurationManager.AppSettings["GenerIconsFolder"];
+        public static string GenreIconsFolderRelative
+        {
+            get
+            {
+                return _genreIconsFolderRelative;
+            }
+        }
+
+        private static readonly string _photosFolderRelative = ConfigurationManager.AppSettings["PhotosFolder"];
+        public static string PhotosFolderRelative
+        {
+            get
+            {
+                return _photosFolderRelative;
+            }
+        }
+    }
+}
---
+++ SpodIgly/Infrastructure/UrlHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+
+namespace SpodIgly.Infrastructure
+{
+    public static class UrlHelper
+    {
+        public static string GenreIconPath(this System.Web.Mvc.UrlHelper helper, string genreIconFilename)
+        {
+            var genreIconFolder = AppConfig.GenreIconsFolderRelative;
+            var path = Path.Combine(genreIconFolder, genreIconFilename);
+            var absolutePath = helper.Content(path);
+            return absolutePath;
+        }
+
+        public static string AlbumCoverPath(this System.Web.Mvc.UrlHelper helper, string albumFilename)
+        {
+            var albumCoverFolder = AppConfig.PhotosFolderRelative;
+            var path = Path.Combine(albumCoverFolder, albumFilename);
+            var absolutePath = helper.Content(path);
+            return absolutePath;
+        }
+    }
+}
